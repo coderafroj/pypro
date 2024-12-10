c60108d233886e5d2cf0cf6d31bce2f4bef22d61
Afu <coderafroj@gmail.com>
2024-12-10 22:12:41 +0530

    chech greater number

---
+++ check-greater-number.py
@@ -0,0 +1,11 @@
+a=int(input("Enter number 1 :-"))
+b=int(input("Enter number 2 :-"))
+c=int(input("Enter number 3 :-"))
+if a>b and b>c:
+    print(a,"is largest")
+elif b>c:
+    print(b,"is largest")
+else:
+    print(c,"is largest")
+
+
